a0323990aa551be285b240d2ce5ff2b4d63f4ec0
Alan Franco <alanfrancob97@gmail.com>
2023-11-12 14:34:55 -0600

    BallController toma en cuenta rotación de cámara
    El script que maneja el control de las bolas ahora toma en cuenta la
    rotación actual de la cámara principal.

--- Assets/Carom/Scripts/BallController.cs
+++ Assets/Carom/Scripts/BallController.cs
@@ -30,9 +30,13 @@ public class BallController : MonoBehaviour
 
         // Mouse click izquierdo soltado
         if ( Input.GetMouseButtonUp(0) == true && objetoSeleccionado.CompareTag("BolaRoja") ) {
+            float rotacionCamaraPrincipalY = camaraPrincipal.transform.eulerAngles.y;
             Vector3 posicionFinalMouse = Input.mousePosition;
             Vector3 distancia = this.posicionInicialMouse - posicionFinalMouse;
             Vector3 fuerza = new(distancia.x, 0, distancia.y);
+
+            // Aplicar la rotación de la cámara principal en el eje Y
+            fuerza = Quaternion.AngleAxis(rotacionCamaraPrincipalY, Vector3.up) * fuerza;
 
             objetoSeleccionadoRigidBody.useGravity = true;
             objetoSeleccionadoRigidBody.AddForce(fuerza * multiplicadorFuerza);
