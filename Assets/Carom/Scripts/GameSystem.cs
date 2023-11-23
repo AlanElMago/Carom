372d165a8e7fac080a64011645d1f0a67b59a09c
Alan Franco <alanfrancob97@gmail.com>
2023-11-23 14:29:59 -0600

    Se implementó condición de perder
    Se implementó la condición de perder cuando no quedan bolas rojas.

--- Assets/Carom/Scripts/GameSystem.cs
+++ Assets/Carom/Scripts/GameSystem.cs
@@ -8,33 +8,48 @@ public class GameSystem : MonoBehaviour
 {
     private enum EstadosJuego { EnProceso, Gano, Perdio }
     private GameObject[] bolasBlancas;
+    private GameObject[] bolasRojas;
     private EstadosJuego estadoJuego = EstadosJuego.EnProceso;
 
-    private DateTime tiempoAparicionMenuNivelCompletado;
+    private DateTime retrasoNivelAcabado;
     private bool eventoGanarActivado = false;
+    private bool eventoPerderActivado = false;
 
-
-    [SerializeField] public GameObject menuNivelComletado;
+    [SerializeField] GameObject menuNivelComletado;
+    [SerializeField] GameObject menuPerdiste;
 
     // Start is called before the first frame update
     void Start()
     {
         this.bolasBlancas = GameObject.FindGameObjectsWithTag("BolaBlanca");
+        this.bolasRojas = GameObject.FindGameObjectsWithTag("BolaRoja");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!AlMenosUnaBolaBlancaEstaActiva() && estadoJuego != EstadosJuego.Gano)
+        if (!AlMenosUnaBolaBlancaEstaActiva() && estadoJuego == EstadosJuego.EnProceso)
         {
-            tiempoAparicionMenuNivelCompletado = DateTime.Now.AddSeconds(1);
+            retrasoNivelAcabado = DateTime.Now.AddSeconds(1);
             estadoJuego = EstadosJuego.Gano;
         }
 
-        if (estadoJuego == EstadosJuego.Gano && DateTime.Now >= tiempoAparicionMenuNivelCompletado && !eventoGanarActivado)
+        if (estadoJuego == EstadosJuego.Gano && DateTime.Now >= retrasoNivelAcabado && !eventoGanarActivado)
         {
             menuNivelComletado.SetActive(true);
             eventoGanarActivado = true;
+        }
+
+        if (!AlMenosUnaBolaRojaEstaActiva() && estadoJuego == EstadosJuego.EnProceso)
+        {
+            retrasoNivelAcabado = DateTime.Now.AddSeconds(1);
+            estadoJuego = EstadosJuego.Perdio;
+        }
+
+        if (estadoJuego == EstadosJuego.Perdio && DateTime.Now >= retrasoNivelAcabado && !eventoPerderActivado)
+        {
+            menuPerdiste.SetActive(true);
+            eventoPerderActivado = true;
         }
     }
 
@@ -43,6 +58,16 @@ public class GameSystem : MonoBehaviour
         foreach(GameObject bolaBlanca in bolasBlancas)
         {
             if (bolaBlanca.activeSelf) { return true; }
+        }
+
+        return false;
+    }
+
+    bool AlMenosUnaBolaRojaEstaActiva()
+    {
+        foreach(GameObject bolaRoja in bolasRojas)
+        {
+            if (bolaRoja.activeSelf) { return true; }
         }
 
         return false;
